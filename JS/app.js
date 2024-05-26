5ec3757b8fc3ec366dc6c20162589f9e771c103d
jarlexdev <jarlexdev@gmail.com>
2024-05-26 15:39:06 -0600

    Agregando logo y favicon

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var links = document.querySelectorAll('header ul li a');
+document.addEventListener("DOMContentLoaded", function () {
+  var links = document.querySelectorAll("header ul li a");
 
-    links.forEach(function(link) {
-        link.addEventListener('click', function() {
-            cerrarMenu();
-        });
+  links.forEach(function (link) {
+    link.addEventListener("click", function () {
+      cerrarMenu();
     });
+  });
 });
 
 function cerrarMenu() {
-    var chk = document.getElementById('chk1');
-    if (chk.checked) {
-        chk.checked = false;
-    }
+  var chk = document.getElementById("chk1");
+  if (chk.checked) {
+    chk.checked = false;
+  }
 }
